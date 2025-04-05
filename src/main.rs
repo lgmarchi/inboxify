7b78d90bc2594535b86ce4d747d4a63247032430
lgmarchi <lucasgmarchi@gmail.com>
2025-04-05 13:25:40 -0300

    feat: integrate database connection into the application startup

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::net::TcpListener;
 
 use inboxify::{configuration::get_configuration, startup::run};
+use sqlx::{Connection, PgConnection};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration =
         get_configuration().expect("Failed to read configuration.");
+    let Ok(connection) =
+        PgConnection::connect(&configuration.database.connection_string())
+            .await
+    else {
+        panic!("Failed to connect to database.");
+    };
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let tcp_listener = TcpListener::bind(address)?;
 
-    run(tcp_listener)?.await
+    run(tcp_listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,21 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    connection: PgConnection,
+) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a web::Data, it uses Arc internally
+    let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
